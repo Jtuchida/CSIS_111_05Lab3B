3f6f1b6adadef9ffc20a71af13c8733bbb6a8f83
Joseph Uchida <jtuchida@liberty.edu>
2021-03-31 13:01:30 -0400

    Added number 61

--- CSIS_111_05Lab3B/CSIS_111_05Lab3B.cpp
+++ CSIS_111_05Lab3B/CSIS_111_05Lab3B.cpp
@@ -11,7 +11,7 @@ int main()
 
 	for (int i = 1; i <= 5; i++) //or i < 6 which stops at 5
 		g1.push_back(i);
-	
+	g1.insert(g1.begin(), 61); //inserts number, 61, into the vectors at the beginning
 	cout << "Numbes in vector: \n";
 	for (int i = 0; i < g1.size(); i++) // numbers in size are going to range from 1 to 5 as in loop above in line 12
 		cout << g1[i] << " ";
